ed995cb9edc2c15164cfdd575273e35272947cd4
TorqueReborn <torquereborn@gmail.com>
2025-12-19 15:55:26 +0530

    posts by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Blog, Category
+
+def posts_by_category(request, category_id):
+    # Fetch the posts that belong to the category with
+    # The id category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    print(context)
+    return render(request, 'posts_by_categories.html', context)
