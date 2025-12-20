3d0f6574381627676cb232ae755b12d7f237509c
TorqueReborn <torquereborn@gmail.com>
2025-12-20 10:08:23 +0530

    blogs and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 
+from assignments.models import About
 from blogs.models import Blog, Category
 
 def home(request):
     posts = Blog.objects.filter(is_featured=False, status='Published')
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
 
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     context = {
         'posts': posts,
         'featured_posts': featured_posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
