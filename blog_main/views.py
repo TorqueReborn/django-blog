33e3b1b7b899904a3c1f5538fd0eae35764ff36a
TorqueReborn <torquereborn@gmail.com>
2025-12-20 11:55:18 +0530

    login and registration

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from assignments.models import About
 from blogs.models import Blog, Category
+
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib import auth
 
 def home(request):
     posts = Blog.objects.filter(is_featured=False, status='Published')
@@ -18,3 +23,33 @@ def home(request):
         'about': about,
     }
     return render(request, 'home.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    form = RegistrationForm()
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
